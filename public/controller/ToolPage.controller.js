d3936258e92733df23d39e5df5e5cf4be5c2cbba
이수용 <leesuyong@isuyong-ui-MacBook-Pro.local>
2019-01-05 18:27:49 +0900

    20180105_1

---
+++ public/controller/ToolPage.controller.js
@@ -0,0 +1,261 @@
+sap.ui.define([
+	'jquery.sap.global',
+	'sap/ui/core/Fragment',
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/json/JSONModel',
+	'sap/m/Popover',
+	'sap/m/Button',
+	"sap/ui/core/UIComponent",
+	"sap/ui/core/mvc/XMLView"
+], function (jQuery, Fragment, Controller, JSONModel, Popover, Button,UIComponent, XMLView) {
+	"use strict";
+
+	var CController = Controller.extend("sap.tnt.sample.ToolPage.ToolPage", {
+		model : new sap.ui.model.json.JSONModel(),
+		data : {
+			navigation: [{
+				title: '수용님',
+				icon: 'sap-icon://employee',
+				expanded: true,
+				key: 'MasterDetail',
+				items: [{
+					title: 'MasterDetail',
+					key: 'MasterDetail'
+				}, {
+					title: 'To Be continue....',
+					key: 'page2'
+				}]
+			}, {
+				title: '승복님',
+				icon: 'sap-icon://building',
+				key: 'root2',
+				items: [{
+					title: 'Child Item 1',
+					key: 'page1'
+				}, {
+					title: 'Child Item 2',
+					key: 'page2'
+				}]
+			}, {
+				title: '두희님	',
+				icon: 'sap-icon://card',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1',
+					key: 'page1'
+				}, {
+					title: 'Child Item 2',
+					key: 'page2'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://action',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://action-settings',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://activate',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://activities',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://add',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://arobase',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://attachment',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://badge',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://basket',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://bed',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Root Item',
+				icon: 'sap-icon://bookmark',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}
+			],
+			fixedNavigation: [{
+				title: 'Fixed Item 1',
+				icon: 'sap-icon://employee'
+			}, {
+				title: 'Fixed Item 2',
+				icon: 'sap-icon://building'
+			}, {
+				title: 'Fixed Item 3',
+				icon: 'sap-icon://card'
+			}],
+			headerItems: [
+			{
+				text: "File"
+			}, {
+				text: "Edit"
+			}, {
+				text: "View"
+			}, {
+				text: "Settings"
+			}, {
+				text: "Help"
+			}]
+		},
+		onInit : function() {
+			this.model.setData(this.data);
+			this.getView().setModel(this.model);
+
+			this._setToggleButtonTooltip(!sap.ui.Device.system.desktop);
+		},
+
+		onItemSelect : function(oEvent) {
+			var item = oEvent.getParameter('item');
+			var viewId = this.getView().getId();
+			// sap.ui.getCore().byId(viewId + "--pageContainer").to(viewId + "--" + item.getKey());
+			XMLView.create({
+				viewName: "sap.ui.expertsui5.root.view.MasterDetail"
+			}).then(function (oView) {
+				oView.placeAt(viewId + "--MasterDetail");
+			});
+		},
+
+		handleUserNamePress: function (event) {
+			var popover = new Popover({
+				showHeader: false,
+				placement: sap.m.PlacementType.Bottom,
+				content:[
+					new Button({
+						text: 'Feedback',
+						type: sap.m.ButtonType.Transparent
+					}),
+					new Button({
+						text: 'Help',
+						type: sap.m.ButtonType.Transparent
+					}),
+					new Button({
+						text: 'Logout',
+						type: sap.m.ButtonType.Transparent
+					})
+				]
+			}).addStyleClass('sapMOTAPopover sapTntToolHeaderPopover');
+
+			popover.openBy(event.getSource());
+		},
+
+		onSideNavButtonPress : function() {
+			var viewId = this.getView().getId();
+			var toolPage = sap.ui.getCore().byId(viewId + "--toolPage");
+			var sideExpanded = toolPage.getSideExpanded();
+
+			this._setToggleButtonTooltip(sideExpanded);
+
+			toolPage.setSideExpanded(!toolPage.getSideExpanded());
+		},
+
+		_setToggleButtonTooltip : function(bLarge) {
+			var toggleButton = this.byId('sideNavigationToggleButton');
+			if (bLarge) {
+				toggleButton.setTooltip('Large Size Navigation');
+			} else {
+				toggleButton.setTooltip('Small Size Navigation');
+			}
+		}
+
+	});
+
+
+	return CController;
+
+});
